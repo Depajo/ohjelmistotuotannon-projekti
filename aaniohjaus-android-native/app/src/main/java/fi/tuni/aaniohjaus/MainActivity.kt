52e3ef5fbb133195d19322a03496d3316ab6fcca
ottokyosti <otto.t.kyosti@tuni.fi>
2023-04-16 17:28:41 +0300

    File renames, service implementation

--- aaniohjaus-android-native/app/src/main/java/fi/tuni/aaniohjaus/MainActivity.kt
+++ aaniohjaus-android-native/app/src/main/java/fi/tuni/aaniohjaus/MainActivity.kt
@@ -3,12 +3,10 @@ package fi.tuni.aaniohjaus
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import fi.tuni.aaniohjaus.ui.theme.AaniohjausTheme
@@ -44,18 +43,24 @@ class MainActivity : ComponentActivity() {
 fun Header() {
     Surface(
         color = Color.Gray,
-        modifier = Modifier.fillMaxWidth().height(50.dp)
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)
     ) {
         Box {
             Button(
                 onClick = {},
-                modifier = Modifier.align(Alignment.TopStart).padding(8.dp)
+                modifier = Modifier
+                    .align(Alignment.TopStart)
+                    .padding(8.dp)
             ) {
                 Text(text = "s")
             }
             Button(
                 onClick = {},
-                modifier = Modifier.align(Alignment.TopEnd).padding(8.dp)
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .padding(8.dp)
             ) {
                 Text(text = "m")
             }
@@ -69,15 +74,23 @@ fun MainContent(location: String) {
         Column(
             Modifier.align(Alignment.Center)
         ) {
+            Image(
+                painter = painterResource(id = R.drawable.sound1_black),
+                contentDescription = null
+            )
+            Spacer(Modifier.height(50.dp))
             Text(
                 text = location,
-                modifier = Modifier.align(Alignment.CenterHorizontally).padding(20.dp, 0.dp)
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(20.dp, 0.dp)
             )
             Spacer(Modifier.height(50.dp))
             Button(
                 onClick = {},
                 modifier = Modifier.size(200.dp, 60.dp),
-                shape = CircleShape
+                shape = CircleShape,
+                colors = ButtonDefaults.buttonColors(Color.DarkGray)
             ) {
                 Text(text = "Toista osoite")
             }
---
+++ aaniohjaus-android-native/app/src/main/java/fi/tuni/aaniohjaus/UpdateLocationService.kt
@@ -0,0 +1,45 @@
+package fi.tuni.aaniohjaus
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import okhttp3.*
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+import kotlin.concurrent.thread
+
+class UpdateLocationService : Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val latitude = intent?.getStringExtra("latitude")
+        val longitude = intent?.getStringExtra("longitude")
+        thread {
+            val client = OkHttpClient.Builder().connectTimeout(20, TimeUnit.SECONDS).build()
+            val request = Request.Builder()
+                .url("https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=$latitude&lon=$longitude")
+                .build()
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    e.printStackTrace()
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    response.use {
+                        if (!response.isSuccessful) throw IOException("Unexpected code $response")
+                        println(response.body!!.string())
+                        Thread.currentThread().interrupt()
+                        stopSelf()
+                    }
+                }
+            })
+        }
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
