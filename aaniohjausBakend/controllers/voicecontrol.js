2bbcd3f2a3e71544efec0a4026b25cd9a5a5643d
Ella Mattila <ella.j.mattila@tuni.fi>
2023-05-02 21:38:27 +0300

    unnecessary comments deleted, renamed objects

--- aaniohjausBakend/controllers/voicecontrol.js
+++ aaniohjausBakend/controllers/voicecontrol.js
@@ -1,15 +1,8 @@
 const katutiedot = require("../models/voicecontrol");
-/*
-const getKatutiedot = async (req, res) => {
-  const response = await katutiedot.findAll();
-  if (response) {
-    res.send(response);
-  }
-};
-*/
-const getBy = async (req, res) => {
+
+const getLocation = async (req, res) => {
   const latitude = req.params.latitude;
-  const longitude = req.params.longitude
+  const longitude = req.params.longitude;
   const response = await katutiedot.findBy(latitude, longitude);
   if (response) {
     res.send(response);
@@ -17,6 +10,5 @@ const getBy = async (req, res) => {
 };
 
 module.exports = {
-  //getKatutiedot,
-  getBy,
+  getLocation,
 };
--- aaniohjausBakend/models/voicecontrol.js
+++ aaniohjausBakend/models/voicecontrol.js
@@ -1,49 +1,18 @@
 const connection = require("../connections/voicecontrol");
 
-/*
-findAll() sisältää yhden testihaun:
-TO DO:
-- longitude ja latitude muuttujiin testiarvojen sijalle käyttäjän sijaintitiedot
-- tällä hetkellä palauttaa lähinnä olevat arvot 0.2km lähin ensimmäisenä, tehtävä jatkokäsittely
-jotta vain lähinnä oleva osoite palautetaan ääneen sanottavaksi
-- ST_Distance_Sphere(Point, Point) * .001 muuntaa etäisyyden kilometreiksi
-*/
-
 const aaniohjaus = {
-  /*
-    findAll: () =>
-        new Promise((resolve, reject) => {
-            var latitude = 61.498139;
-            var longitude = 23.7515;
-            connection.query(
-                `SELECT *, ST_Distance_Sphere(POINT(${longitude}, ${latitude}), POINT(longitude, latitude)) * .001 AS distance_in_kms
-      FROM katutiedot
-      HAVING distance_in_kms <= 0.5
-      ORDER BY distance_in_kms ASC;`,
-                (err, result) => {
-                    if (err) {
-                        reject(err);
-                    }
-                    resolve(result);
-                }
-            );
-        }),
-
-  */
-  /// findby koordinaatit (latitude and longitude)
-
   findBy: (latitude, longitude) =>
     new Promise((resolve, reject) => {
       console.log(latitude, longitude);
       let query = `SELECT latitude, longitude, katu, katunumero, a.postinumero,k.kunta, ST_Distance_Sphere(POINT(?, ?), POINT(longitude, latitude)) * .001
-AS distance_in_kms
-FROM katutiedot
-INNER JOIN alue a
-ON katutiedot.alue_id = a.alue_id
-INNER JOIN kunta k
-ON a.kunta_id = k.kunta_id
-HAVING distance_in_kms <= 0.2
-ORDER BY distance_in_kms ASC;`;
+      AS distance_in_kms
+      FROM katutiedot
+      INNER JOIN alue a
+      ON katutiedot.alue_id = a.alue_id
+      INNER JOIN kunta k
+      ON a.kunta_id = k.kunta_id
+      HAVING distance_in_kms <= 0.2
+      ORDER BY distance_in_kms ASC;`;
       connection.query(query, [longitude, latitude], (err, result) => {
         if (err) {
           reject(err);
