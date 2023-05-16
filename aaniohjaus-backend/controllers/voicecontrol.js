dc4a76809d83ecc997c395252a4556524b9ccef1
Ella Mattila <ella.j.mattila@tuni.fi>
2023-05-16 09:53:02 +0300

    code commented

--- aaniohjaus-backend/controllers/voicecontrol.js
+++ aaniohjaus-backend/controllers/voicecontrol.js
@@ -1,5 +1,13 @@
-const katutiedot = require("../models/voicecontrol");
+const addressInfo = require("../models/voicecontrol");
 const Joi = require('joi');
+/**
+ * getLocation 1st validates parameters it get's as request from /routes,
+ * 2nd makes the database call with validated parameters.
+ * Returns either data from database or statuscode:
+ * 400 = Bad request, parameter validation failed
+ * 204 = No content, database call didn't return data
+ * 500 = Internal server error, something failed during sending data
+ */
 
 const getLocation = async (req, res) => {
 	const schema = Joi.object({
@@ -14,7 +22,7 @@ const getLocation = async (req, res) => {
 		const latitude = validation.value.latitude;
 		const longitude = validation.value.longitude;
 		try {
-			const response = await katutiedot.findBy(latitude, longitude);
+			const response = await addressInfo.findBy(latitude, longitude);
 			if (response) {
 				if (response.length == 0) {
 					res.sendStatus(204);
